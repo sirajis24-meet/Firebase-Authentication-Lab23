e1efebccf4d004ffee2d0eec989d812e54ae1f3f
sirajis24-meet <139681226+sirajis24-meet@users.noreply.github.com>
2023-07-23 18:28:18 +0300

    Update app.py

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -2,6 +2,30 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
 
+
+
+
+Config = {
+
+  "apiKey": "AIzaSyAm3lbYU01er9JDLIXxLCWcHdrgmNCRu_0",
+
+  "authDomain": "sheeple-85021.firebaseapp.com",
+
+  "projectId": "sheeple-85021",
+
+  "storageBucket": "sheeple-85021.appspot.com",
+
+  "messagingSenderId": "630439104792",
+
+  "appId": "1:630439104792:web:848393d59ae654ced58e6d",
+
+  "databaseURL" : "",
+
+}
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth() 
+
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
@@ -13,6 +37,9 @@ def signin():
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        email = request.form('email')
+        password = request.form('password')
     return render_template("signup.html")
 
 
