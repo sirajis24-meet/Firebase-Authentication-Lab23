205450c5526cac31d8ca3111178aa45a4844d222
sirajis24-meet <139681226+sirajis24-meet@users.noreply.github.com>
2023-07-25 09:50:39 +0300

    e

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -10,11 +10,12 @@ Config = {
   "storageBucket": "sheeple-85021.appspot.com",
   "messagingSenderId": "630439104792",
   "appId": "1:630439104792:web:848393d59ae654ced58e6d",
-  "databaseURL" : "",
+  "databaseURL" : "https://sheeple-85021-default-rtdb.europe-west1.firebasedatabase.app/",
 }
 
 firebase = pyrebase.initialize_app(Config)
 auth = firebase.auth() 
+db = firebase.database()
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
@@ -41,8 +42,14 @@ def signup():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
+        fullname = request.form['fullname']
+        username = request.form['username']
+        bio = request.form['bio']
         try:
             login_session['user'] = auth.create_user_with_email_and_password(email, password)
+            UID = login_session['user']['localId']
+            user = {'fullname' : fullname, 'username' : username, 'bio' : bio}
+            db.child("Users").child(UID).set(user)
             return redirect(url_for('add_tweet'))
         except:
             error = "auth failed"
@@ -51,7 +58,22 @@ def signup():
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
-    return render_template("add_tweet.html")
+    error = ""
+    if request.method == "POST" :
+        Text = request.form['Text1']
+        Title = request.form['Title']
+        try:
+            UID = login_session['user']['localId']
+            tweet = {'Title' : Title, 'Text' : Text, 'UID' : UID}
+            db.child("Tweets").push(tweet)
+            return redirect(url_for('all_tweets'))
+        except:
+            error = "coudnt add"
+    return render_template("add_tweet.html", error = error)
+
+@app.route('/all_tweets')
+def all_tweets():
+    return render_template("Tweets.html", tweets = db.child('Tweets').get().val())
 
 
 if __name__ == '__main__':
